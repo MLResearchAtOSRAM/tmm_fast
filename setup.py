97e942a19cbffb56a8e09357d875ad941ad9cd61
Alexander Luce <Alexander.Luce@osram-os.com>
2022-10-31 10:22:13 +0100

    deleted old files

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(
-    name='tmmfast',
+    name='tmm_fast',
     version='0.1',
     description='tmm_fast is a lightweight package to speed up optical planar multilayer thin-film device computation.',
     url='https://github.com/MLResearchAtOSRAM/tmm_fast',
